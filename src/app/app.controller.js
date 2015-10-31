aad9d444387efa7f0aeee4416e9ccd27bfc7f550
David Symons <symons@aospace.com>
2015-10-31 12:51:59 -0700

    adding logout functionality

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -8,21 +8,31 @@
     AppController.$inject = ['$scope', '$log', '$location', 'steelfigAuth', 'steelfigService'];
     function AppController ($scope, $log, $location, steelfigAuth, steelfig) {
         $scope.$log = $log;
+        $scope.logout = logout;
 
-        // TODO: don't leave this here!!
-        $scope.eventId = 1;
-        localStorage.setItem('eventId', 1);
+        activate();
 
-        // Force login
-        if (!steelfigAuth.loadToken()) {
-            return $location.path('login');
+        function activate () {
+            // TODO: don't leave this here!!
+            $scope.eventId = 1;
+            localStorage.setItem('eventId', 1);
+
+            // Force login
+            if (!steelfigAuth.loadToken()) {
+                return $location.path('login');
+            }
+
+            steelfig.user.fetch()
+                .then(function (data) {
+                    $scope.account = data.account;
+                    $scope.user = data.user;
+
+                });
         }
 
-        steelfig.user.fetch()
-            .then(function (data) {
-                $scope.account = data.account;
-                $scope.user = data.user;
-
-            });
+        function logout () {
+            localStorage.clear();
+            $location.path('login');
+        }
     }
 })();
