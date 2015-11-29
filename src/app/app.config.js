5ba42b599f7402d076d6586c6c7048bc8f489e12
Dave Symons <symons@aospace.com>
2015-11-29 11:14:51 -0800

    adding no-cache headers

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -15,7 +15,14 @@
 
     configureHttp.$inject = ['$httpProvider'];
     function configureHttp ($httpProvider) {
+        if (!$httpProvider.defaults.headers.common) {
+            $httpProvider.defaults.headers.common = {};
+        }
+
         $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
+        $httpProvider.defaults.headers.common.Pragma = "no-cache";
+        $httpProvider.defaults.headers.common["If-Modified-Since"] = "0";
     }
 
     configureSceDelegate.$inject = ['$sceDelegateProvider'];
